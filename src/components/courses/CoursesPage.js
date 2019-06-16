9f8cae75eff2c366aa468b43125c4719ec739781
Julius Philip Bartolome <juliusbartolome@hotmail.ph>
2019-06-16 14:10:19 +0800

    modified course page

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,39 +5,15 @@ import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import { bindActionCreators } from "redux";
 class CoursesPage extends React.Component {
-  state = {
-    course: {
-      title: ""
-    }
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.actions.createCourse(this.state.course);
-  };
-
-  handleChange = event => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course });
-  };
-
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <>
         <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        />
-
-        <input type="submit" value="Save" />
 
         {this.props.courses.map(course => (
           <div key={course.title}>{course.title}</div>
         ))}
-      </form>
+      </>
     );
   }
 }
