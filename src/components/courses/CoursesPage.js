f00d5aa22333d7f496d30c971b1ecf1467ad1018
Julius Philip Bartolome <juliusbartolome@hotmail.ph>
2019-06-18 22:34:04 +0800

    added course list in course page

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 
 import * as courseActions from "../../redux/actions/courseActions";
 import { bindActionCreators } from "redux";
+import CourseList from "./CourseList";
 class CoursesPage extends React.Component {
   componentDidMount() {
     this.props.actions.loadCourses().catch(error => {
@@ -15,10 +16,7 @@ class CoursesPage extends React.Component {
     return (
       <>
         <h2>Courses</h2>
-
-        {this.props.courses.map(course => (
-          <div key={course.title}>{course.title}</div>
-        ))}
+        <CourseList courses={this.props.courses} />
       </>
     );
   }
